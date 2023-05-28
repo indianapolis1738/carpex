d43de96b520357bfbde7cba69618841ea646b941
mistercarp <77747892+mistercarp@users.noreply.github.com>
2023-05-28 04:10:40 +0100

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,155 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './Images/CEX.jpg';
+import flag from './Images/nigeria.png';
+import box from './Images/New.png';
+import iphone from './Images/pink-iphone.png'
+import icon from './Images/icon-step-1.png'
 import './App.css'
+import SlideShow from './Slideshow';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav className='navigation'>
+          <img src={logo} className="logo" alt="React logo" />
+
+          <div className='nav-links'>
+            <img src={flag} className='flag' alt='flag' />
+            <p>Market</p>
+            <p><a href='https://wa.link/07fs30'>
+            Trade
+            </a></p>
+            <p>Blog</p>
+            <p>About Us</p>
+            
+          </div>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='section'>
+        <div>
+          <p className='bold-buy'>
+            Sell and Buy <br />
+            Crypto with ease.
+          </p>
+          <p className='regular-buy'>
+            Buy and sell Bitcoin, Ethereum, XRP and many other cryptocurrencies with fiat , cryptocurrencies or your national currency 
+          </p>
+          <div className='input-field'>
+            <input type="email" name="email" id="email" placeholder='Enter e-mail address here.' />
+            <button type="submit">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <img
+          src={box}
+          alt='box'
+          className='box'
+        />
+      </div>
+      <div className='blue-box'>
+        <p className='yellow'>SECURELY BUY, SELL and TRACK</p>
+        <p className='p-trade'>Trade any cryptocurrency of your choice with fiat currency using bank transfer.</p>
+        <table className='table-div'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Change($)</th>
+              <th>Trade</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
+      </div>
+      <div className="trusted-div">
+        <div className='trusted-plat'>
+          <p className='a-trusted'>
+            A trusted Cryptocurrency <br /> trading Platform
+          </p>
+          <p className="small-cap">
+            Why Carp Exchange?
+          </p>
+        </div>
+        <div className="lines">
+          <SlideShow/>
+        </div>
+      </div>
+      <div className="how-to">
+        <p className="how-to-p">
+          How to trade with confidence
+        </p>
+        <p className="begin">
+          Begin your trading journey with these easy steps
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="steps">
+        <div className="items-step">
+          <div className="first-item">
+            <img src={icon} alt="icon" className="icon" />
+            <div className="icon-content">
+              <p className="bold-icon-content">
+                Create your account
+              </p>
+              <p className="small-icon-content">
+                Enter your e-mail adress and other sign up details to  create your account successfully.
+              </p>
+            </div>
+          </div>
+          <div className="first-item">
+            <img src={icon} alt="icon" className="icon" />
+            <div className="icon-content">
+              <p className="bold-icon-content">
+                Create your account
+              </p>
+              <p className="small-icon-content">
+                Enter your e-mail adress and other sign up details to  create your account successfully.
+              </p>
+            </div>
+          </div>
+          <div className="first-item">
+            <img src={icon} alt="icon" className="icon" />
+            <div className="icon-content">
+              <p className="bold-icon-content">
+                Create your account
+              </p>
+              <p className="small-icon-content">
+                Enter your e-mail adress and other sign up details to  create your account successfully.
+              </p>
+            </div>
+          </div>
+          <div className="first-item">
+            <img src={icon} alt="icon" className="icon" />
+            <div className="icon-content">
+              <p className="bold-icon-content">
+                Create your account
+              </p>
+              <p className="small-icon-content">
+                Enter your e-mail adress and other sign up details to  create your account successfully.
+              </p>
+            </div>
+          </div>
+          <button className='whatsapp'>
+            <a href='https://wa.link/07fs30'>
+            Get Started
+            </a>
+          </button>
+        </div>
+        <img
+          src={iphone}
+          alt='phone'
+          className='phone'
+        />
+      </div>
+      <footer className='footer-item'>
+        Carp Exchange 2023. ALL RIGHTS RESERVED
+      </footer>
     </>
   )
 }
