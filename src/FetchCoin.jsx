9a0c0e51151a9a7f2aef0fa4047a06f08bc49e84
mistercarp <77747892+mistercarp@users.noreply.github.com>
2023-05-31 13:55:35 +0100

    First Commit

--- src/FetchCoin.jsx
+++ src/FetchCoin.jsx
@@ -11,7 +11,7 @@ function FetchCoin() {
     useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false&locale=en"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&locale=en"
       )
       .then((res) => {
         setCoin(res.data);
