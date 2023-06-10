4b73e6d26b6dfe40960c44d75f4743595e4c5f47
mistercarp <77747892+mistercarp@users.noreply.github.com>
2023-06-10 08:00:33 +0100

    First Commit

--- src/Supabase/SupaBase.js
+++ src/Supabase/SupaBase.js
@@ -2,7 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://wdmqfqwhaeaensfuurcy.supabase.co";
 
-const supabaseKey = `${import.meta.env.VITE_REACT_APP_ANON_KEY}`;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbXFmcXdoYWVhZW5zZnV1cmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzNDE4MDYsImV4cCI6MjAwMTkxNzgwNn0.8Fchbn0naXjN-mHjsrqV2l4o9clnv7LUb1XKtP9DbMg";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
